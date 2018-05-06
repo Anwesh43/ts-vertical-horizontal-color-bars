36475dac8caf7c058a28ab5505ed82a9951ec95f
Anwesh43 <anweshthecool0@gmail.com>
2018-05-06 12:45:15 +0530

    adding VHCAnimator class

--- VertHoriColorBarStage.ts
+++ VertHoriColorBarStage.ts
@@ -54,5 +54,27 @@ class VHCState {
             startcb()
         }
     }
+}
 
+class VHCAnimator {
+
+    animated : boolean = false
+
+    interval : number
+    
+    start(updatecb : Function) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            }, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
 }
