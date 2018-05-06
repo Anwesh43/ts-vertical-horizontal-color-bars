e300012aa5b9f6fc98d57a2a26be7d00c685727a
Anwesh43 <anweshthecool0@gmail.com>
2018-05-06 12:42:18 +0530

    adding VHCState class

--- VertHoriColorBarStage.ts
+++ VertHoriColorBarStage.ts
@@ -25,7 +25,34 @@ class VertHoriColorBarStage {
 
     handleTap() {
         this.canvas.onmousedown = (event) => {
-            
+
         }
     }
+}
+
+class VHCState {
+
+    scale : number = 0
+
+    dir : number = 0
+
+    prevScale : number = 0
+
+    update(stopcb : Function) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+
+    startUpdating(startcb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+
 }
